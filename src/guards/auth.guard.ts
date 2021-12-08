fdc326b232bb63e4e894ccb8984b19b4ff4b097d
tyyin <lantaiying@gmail.com>
2021-12-08 17:54:59 +0800

    Guard介绍

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,28 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+
+const logger = new Logger('auth.guard');
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+
+    logger.log(roles);
+
+    if (!roles) {
+      return true;
+    }
+    return true;
+  }
+}
